970a15d4231b8b97dea02eb5c3688a1209f22523
Aaron <highland@seattleu.edu>
2016-05-29 21:58:24 -0700

    update
    worked on the main.js file more. Needing to bind the select radio event
    to the desired states.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,46 @@
-//Script to turn the element into a map
-$(document).ready(function() {
-    $('#map').usmap({});
+
+$(document).ready(function () {
+    
+    //Script to turn the element into a map
+    $('#map').usmap({
+ 
+        stateStyles: {
+            fill: 'lightgreen'
+        },
+        
+    clickState: {
+    'WA' : function(event, data) {
+      console.log('You interacted with the state of Washington');
+    }
+  },
+        
+        stateSpecificStyles: {
+          'WA' : {fill: 'purple'},  
+          'OR' : {fill: 'purple'},
+          'ID' : {fill: 'purple'},    
+        
+        },
+        
+        
+    
+    });  
+    
+
+    //select radio button to call event
+    $(".radio").click(function ( ){
+        alert("highlight states")
+        
+      
+    });
+
+    
+    
+    
+    
+    
+    
+    
+    
   });
+
+
